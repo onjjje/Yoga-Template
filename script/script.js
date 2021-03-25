32547105ea536e6cd9a420ccc3bf881e2977e419
Govorushkin Nikodim <onjjje@gmail.com>
2021-03-25 11:43:57 +0300

    fixed home page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// video 
 var overlay = document.getElementById('overlay');
 var vid = document.getElementById('video');
 
@@ -16,3 +17,24 @@ function play() {
         overlay.className = "";
     }
 }
+// scroll href 
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('.scrollto').offsetHeight;
+        // const topOffset = 0; // если не нужен отступ сверху 
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
